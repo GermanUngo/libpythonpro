556e1a5eb12050bc1376912782056e2a73b3a39e
GERMAN IGNACIO UNGO CORRALES <germanungo@gmail.com>
2022-09-01 21:59:10 -0300

    Criado setup.py, pronto para release 1.0 class #18

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro"
+PACKAGE = "libpythonpro_ger"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "German Ungo"
